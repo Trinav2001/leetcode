8fb9cd7a68d198fb87ba182ec2ffec7acb191930
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-04-24 13:26:35 +0530

    Time: 3 ms (53.97%), Space: 9.8 MB (48.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> m;
+        for(auto c : s) {
+            m[c]++;
+        }
+        for(auto c : t) {
+            m[c]--;
+        }
+        for(auto& key : m) {
+            if(key.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
