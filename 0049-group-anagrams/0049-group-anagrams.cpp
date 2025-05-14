413490807f483179ead0060228df76ad7e89ad8d
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-14 14:06:45 +0530

    Time: 175 ms (5.03%), Space: 83.8 MB (5.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Method - 2
         unordered_map<string, vector<string>> map;
         for(auto s : strs) {
-            string key = s;
-            sort(key.begin(), key.end());
+            vector<int> count(26, 0);
+            for(auto c : s) {
+                count[c - 'a']++;
+            }
+            string key = "";
+            for(auto i : count) {
+                key = key + to_string(i) + ",";
+            }
             map[key].push_back(s);
         }
         vector<vector<string>> ans;
@@ -12,5 +19,19 @@ public:
             ans.push_back(pair.second);
         }
         return ans;
+
+        
+        // Method - 1
+        // unordered_map<string, vector<string>> map;
+        // for(auto s : strs) {
+        //     string key = s;
+        //     sort(key.begin(), key.end());
+        //     map[key].push_back(s);
+        // }
+        // vector<vector<string>> ans;
+        // for(auto& pair : map) {
+        //     ans.push_back(pair.second);
+        // }
+        // return ans;
     }
 };
