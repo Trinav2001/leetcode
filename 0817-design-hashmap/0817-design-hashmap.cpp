2bf288498f0ffd7a5550eda2a9cc61714410aa55
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-15 15:33:14 +0530

    Time: 21 ms (69.24%), Space: 61 MB (59.76%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,22 +1,46 @@
 class MyHashMap {
 private:
-    vector<int> map;
+    static const int SIZE = 1000;
+    vector<list<pair<int, int>>> table;
+
+int hash(int key) {
+    return (key % SIZE);
+}
 
 public:
     MyHashMap() {
-        map.resize(1000001, -1);
+        table.resize(SIZE);
     }
     
     void put(int key, int value) {
-        map[key] = value;
+        int index = hash(key);
+        for(auto& [k, v] : table[index]) {
+            if(k == key) {
+                v = value;
+                return;
+            }
+        }
+        table[index].push_back({key, value});
     }
     
     int get(int key) {
-        return map[key];
+        int index = hash(key);
+        for(auto& [k,v] : table[index]) {
+            if(k == key) {
+                return v;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        map[key] = -1;
+        int index = hash(key);
+        for(auto it = table[index].begin(); it != table[index].end(); it++) {
+            if(it->first == key) {
+                table[index].erase(it);
+                return;
+            }
+        }
     }
 };
 
