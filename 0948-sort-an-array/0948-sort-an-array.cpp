af5b481e4d0765da2e12583913f3e197247a60bc
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-19 01:04:33 +0530

    Time: 19 ms (89.67%), Space: 85.1 MB (48.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -9,31 +9,99 @@ public:
         // return nums;
         // SelectionSort(nums);
         // return nums;
-        CountSort(nums);
-        return nums;
-    }
-
-    void CountSort(vector<int>& nums) {
-        unordered_map<int, int> map;
-        int min = *min_element(nums.begin(), nums.end());
-        int max = *max_element(nums.begin(), nums.end());
-
-        for(auto& val : nums) {
-            map[val]++;
-        }
-
-        int index = 0;
-        for(int val = min; val <= max; ) {
-            if(map[val] > 0) {
-                nums[index++] = val;
-                map[val]--;
+        // CountSort(nums);
+        // return nums;
+        
+        vector<int> negatives, positives;
+        for(auto& num : nums) {
+            if(num < 0) {
+                negatives.push_back(-num);
             }
             else {
-                val++;
+                positives.push_back(num);
             }
         }
 
+        if(!negatives.empty()) {
+            RadixSort(negatives);
+            reverse(negatives.begin(), negatives.end());
+            for(auto& num : negatives) {
+                num = -num;
+            }
+        }
+
+        if(!positives.empty()) {
+            RadixSort(positives);
+        }
+
+        nums.clear();
+        for(auto& num : negatives) {
+            nums.push_back(num);
+        }
+
+        for(auto& num : positives) {
+            nums.push_back(num);
+        }
+
+        return nums;
+
+
     }
+
+    void RadixSort(vector<int>& nums) {
+        int n = nums.size();
+        int max = *max_element(nums.begin(), nums.end());
+        int exp = 1;
+        while(max / exp > 0) {
+            CountSort(nums, n, exp);
+            exp = exp * 10;
+        }
+    }
+
+    void CountSort(vector<int>& nums, int n, int exp) {
+        vector<int> count(10, 0);
+        for(int i = 0; i < n; i++) {
+            int digit = (nums[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        for(int i = 1; i < count.size(); i++) {
+            count[i] = count[i] + count[i-1];
+        }
+
+        vector<int> output(n);
+
+        for(int i = n - 1; i >= 0; i--) {
+            int digit = (nums[i] / exp) % 10;
+            output[--count[digit]] = nums[i];
+        }
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = output[i];
+        }
+    }
+
+
+    // void CountSort(vector<int>& nums) {
+    //     unordered_map<int, int> map;
+    //     int min = *min_element(nums.begin(), nums.end());
+    //     int max = *max_element(nums.begin(), nums.end());
+
+    //     for(auto& val : nums) {
+    //         map[val]++;
+    //     }
+
+    //     int index = 0;
+    //     for(int val = min; val <= max; ) {
+    //         if(map[val] > 0) {
+    //             nums[index++] = val;
+    //             map[val]--;
+    //         }
+    //         else {
+    //             val++;
+    //         }
+    //     }
+    // }
 
     // void SelectionSort(vector<int>& nums) {
     //     int i;
