b19555e2917508980cf810bf7fc3f404533211a5
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-20 14:29:07 +0530

    Time: 3 ms (51.77%), Space: 17.8 MB (91.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Method - 2
         unordered_map<int, int> map;
-        for(auto& key : nums) {
-            map[key]++;
+        for(auto& val : nums) {
+            map[val]++;
         }
 
-        vector<pair<int, int>> array;
-        for(auto& [k, v] : map) {
-            array.push_back({v ,k});
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for(auto& entry : map) {
+            heap.push({entry.second, entry.first}); 
+            if(heap.size() > k) {
+                heap.pop();
+            }
         }
 
-        sort(array.rbegin(), array.rend());
         vector<int> output;
-        for(int i = 0; i < k; i++) {
-            output.push_back(array[i].second);
+        while(!heap.empty()) {
+            output.push_back(heap.top().second);
+            heap.pop();
         }
         return output;
+
+
+
+        // Method - 1
+        // unordered_map<int, int> map;
+        // for(auto& key : nums) {
+        //     map[key]++;
+        // }
+
+        // vector<pair<int, int>> array;
+        // for(auto& [k, v] : map) {
+        //     array.push_back({v ,k});
+        // }
+
+        // sort(array.rbegin(), array.rend());
+        // vector<int> output;
+        // for(int i = 0; i < k; i++) {
+        //     output.push_back(array[i].second);
+        // }
+        // return output;
     }
 };
