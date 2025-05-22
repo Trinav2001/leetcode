7f45a84bfd8f256eecac40adb20db22fbce1edcd
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-22 20:32:13 +0530

    Time: 3 ms (32.69%), Space: 19.8 MB (22.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
-        for(int i = 1; i < prices.size(); i++) {
-            if(prices[i] > prices[i-1]) {
-                profit += (prices[i] - prices[i-1]);
-            }
+
+        // Method - 2 DP
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2,0));
+        dp[0][0] = 0;           // Not holding
+        dp[0][1] = -prices[0];  // Holding
+
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
-        return profit;
+
+        return dp[n-1][0];
+        // int profit = 0;
+        // for(int i = 1; i < prices.size(); i++) {
+        //     if(prices[i] > prices[i-1]) {
+        //         profit += (prices[i] - prices[i-1]);
+        //     }
+        // }
+        // return profit;
     }
 };
