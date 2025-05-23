be25103599669263fbc6e2797419c33eb7e30a38
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-23 16:37:57 +0530

    Time: 0 ms (100%), Space: 23.6 MB (27.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,61 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> map;
-        vector<int> output;
-        int n = nums.size();
-        int x = n / 3;
-        for(auto& num : nums) {
-            map[num]++;
-        }
+        
+        // Boyer-Moore Voting Algorithm
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = -1000000001;
+        int candidate2 = -1000000002;
 
-        for(auto& pair : map) {
-            if(pair.second > x) {
-                output.push_back(pair.first);
+        vector<int> output;
+        
+        for(auto& num : nums) {
+            if(num == candidate1) {
+                count1++;
+            }
+            else if(num == candidate2) {
+                count2++;
+            }
+            else if(count1 == 0) {
+                candidate1 = num;
+                count1++;
+            }
+            else if(count2 == 0) {
+                candidate2 = num;
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
+        count1 = 0;
+        count2 = 0;
+
+        for(auto& num : nums) {
+            if(num == candidate1) count1++;
+            if(num == candidate2) count2++;
+        }
+
+        if(count1 > nums.size()/3) output.push_back(candidate1);
+        if(count2 > nums.size()/3) output.push_back(candidate2);
         return output;
+
+        // Method - 1
+        // unordered_map<int, int> map;
+        // vector<int> output;
+        // int n = nums.size();
+        // int x = n / 3;
+        // for(auto& num : nums) {
+        //     map[num]++;
+        // }
+
+        // for(auto& pair : map) {
+        //     if(pair.second > x) {
+        //         output.push_back(pair.first);
+        //     }
+        // }
+        // return output;
     }
 };
