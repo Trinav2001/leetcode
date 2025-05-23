ed2e4e840f369cc2a4029478dbd2bc08f9e31729
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-23 16:50:07 +0530

    Time: 12 ms (7.29%), Space: 26.5 MB (6.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,45 +2,76 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        // Boyer-Moore Voting Algorithm
-        int count1 = 0;
-        int count2 = 0;
-        int candidate1 = -1000000001;
-        int candidate2 = -1000000002;
+        // Boyer-Moore Voting Algorithm using HashMap
+
+        unordered_map<int, int> map;
+        for(auto& num: nums) {
+            map[num]++;
+        }
+
+        if(map.size() > 2) {
+            unordered_map<int, int> temp;
+            for(auto& pair : map) {
+                if(pair.second > 1) {
+                    temp[pair.first] = pair.second - 1;
+                }
+            }
+            map = temp;
+        }
 
         vector<int> output;
-        
-        for(auto& num : nums) {
-            if(num == candidate1) {
-                count1++;
+        for(auto& pair : map) {
+            int frequency = 0;
+            for(auto& num : nums) {
+                if(num == pair.first) frequency++;
             }
-            else if(num == candidate2) {
-                count2++;
-            }
-            else if(count1 == 0) {
-                candidate1 = num;
-                count1++;
-            }
-            else if(count2 == 0) {
-                candidate2 = num;
-                count2++;
-            }
-            else {
-                count1--;
-                count2--;
-            }
-        }
-        count1 = 0;
-        count2 = 0;
-
-        for(auto& num : nums) {
-            if(num == candidate1) count1++;
-            if(num == candidate2) count2++;
+            if(frequency > nums.size()/3) output.push_back(pair.first);
         }
 
-        if(count1 > nums.size()/3) output.push_back(candidate1);
-        if(count2 > nums.size()/3) output.push_back(candidate2);
         return output;
+
+
+
+
+        // Boyer-Moore Voting Algorithm
+        // int count1 = 0;
+        // int count2 = 0;
+        // int candidate1 = -1000000001;
+        // int candidate2 = -1000000002;
+
+        // vector<int> output;
+        
+        // for(auto& num : nums) {
+        //     if(num == candidate1) {
+        //         count1++;
+        //     }
+        //     else if(num == candidate2) {
+        //         count2++;
+        //     }
+        //     else if(count1 == 0) {
+        //         candidate1 = num;
+        //         count1++;
+        //     }
+        //     else if(count2 == 0) {
+        //         candidate2 = num;
+        //         count2++;
+        //     }
+        //     else {
+        //         count1--;
+        //         count2--;
+        //     }
+        // }
+        // count1 = 0;
+        // count2 = 0;
+
+        // for(auto& num : nums) {
+        //     if(num == candidate1) count1++;
+        //     if(num == candidate2) count2++;
+        // }
+
+        // if(count1 > nums.size()/3) output.push_back(candidate1);
+        // if(count2 > nums.size()/3) output.push_back(candidate2);
+        // return output;
 
         // Method - 1
         // unordered_map<int, int> map;
