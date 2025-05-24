f238cf649332d2e29d4c8cec020c4057f04f046c
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-24 18:00:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (30.67%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -3,18 +3,17 @@ public:
     string mergeAlternately(string word1, string word2) {
         string output = "";
         int i = 0;
-        int j = 0;
 
-        while(i < word1.length() || j < word2.length()) {
+
+        while(i < word1.length() || i < word2.length()) {
             if(i < word1.length()) {
                 output += word1[i];
-                i++;
             }
 
-            if(j < word2.length()) {
-                output += word2[j];
-                j++;
+            if(i < word2.length()) {
+                output += word2[i];
             }
+            i++;
         }
 
 
