4d5926d8d8286e94384fd4e9e3f0443b38b00ca8
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-24 17:58:46 +0530

    Time: 4 ms (31.94%), Space: 8.5 MB (30.67%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -5,23 +5,18 @@ public:
         int i = 0;
         int j = 0;
 
-        while(i < word1.length() && j < word2.length()) {
-            if(i == j) {
+        while(i < word1.length() || j < word2.length()) {
+            if(i < word1.length()) {
                 output += word1[i];
                 i++;
             }
-            else {
+
+            if(j < word2.length()) {
                 output += word2[j];
                 j++;
             }
         }
 
-        for(; i < word1.length(); i++) {
-            output += word1[i];
-        }
-        for(; j < word2.length(); j++) {
-            output += word2[j];
-        }
 
         return output;
     }
