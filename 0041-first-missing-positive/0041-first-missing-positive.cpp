5f8b6375fc63f5d4269c0c9a1eef8937e0bb7c2b
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-24 11:02:56 +0530

    Time: 0 ms (100%), Space: 55 MB (74.45%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int, int> count;
-        for(auto& num : nums) {
-            count[num]++;
+        
+        // Method - 2
+        int i = 0;
+        while(i < nums.size()) {
+            if(nums[i] > 0 && 
+            nums[i] <= nums.size() && 
+            nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+            else {
+                i++;
+            }
+            
         }
-        int max = *max_element(nums.begin(), nums.end());
-        int i;
-        for(i = 1; i <= max; i++) {
-            if(count[i] == 0) {
-                return i;
+
+        for(i = 0; i < nums.size(); i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
             }
         }
-        return i;
+        return i + 1;
     }
+
+    void swap(int& a, int& b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+        // Method - 1
+        // unordered_map<int, int> count;
+        // for(auto& num : nums) {
+        //     count[num]++;
+        // }
+        // int max = *max_element(nums.begin(), nums.end());
+        // int i;
+        // for(i = 1; i <= max; i++) {
+        //     if(count[i] == 0) {
+        //         return i;
+        //     }
+        // }
+        // return i;
 };
