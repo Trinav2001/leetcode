38b0f73ecd7b8e2d6a4a5fe99d29eee2652f9512
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-24 13:03:26 +0530

    Time: 4 ms (44.87%), Space: 55.1 MB (46.37%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,33 +2,66 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         
-        // Method - 2
-        int i = 0;
-        while(i < nums.size()) {
-            if(nums[i] > 0 && 
-            nums[i] <= nums.size() && 
-            nums[i] != nums[nums[i] - 1]) {
-                swap(nums[i], nums[nums[i] - 1]);
+
+        // Method - 3 Negative indexing
+        for(auto& num : nums) {
+            if(num < 0) {
+                num = 0;
             }
-            else {
-                i++;
-            }
-            
         }
 
+        for(int i = 0; i < nums.size(); i++) {
+            int val = abs(nums[i]);
+            if(val > 0 && val <= nums.size() && nums[val - 1] >= 0) {
+                if(nums[val - 1] == 0) {
+                    nums[val - 1] = -(nums.size() + 1);
+                }
+                else {
+                    nums[val - 1] = -(nums[val - 1]);
+                }
+            }
+        }
+
+
+        int i;
         for(i = 0; i < nums.size(); i++) {
-            if(nums[i] != i + 1) {
+            if(nums[i] >= 0) {
                 return i + 1;
             }
         }
+
         return i + 1;
     }
 
-    void swap(int& a, int& b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
+
+
+        // Method - 2
+    //     int i = 0;
+    //     while(i < nums.size()) {
+    //         if(nums[i] > 0 && 
+    //         nums[i] <= nums.size() && 
+    //         nums[i] != nums[nums[i] - 1]) {
+    //             swap(nums[i], nums[nums[i] - 1]);
+    //         }
+    //         else {
+    //             i++;
+    //         }
+            
+    //     }
+
+    //     for(i = 0; i < nums.size(); i++) {
+    //         if(nums[i] != i + 1) {
+    //             return i + 1;
+    //         }
+    //     }
+    //     return i + 1;
+    // }
+
+    // void swap(int& a, int& b) {
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
 
         // Method - 1
         // unordered_map<int, int> count;
