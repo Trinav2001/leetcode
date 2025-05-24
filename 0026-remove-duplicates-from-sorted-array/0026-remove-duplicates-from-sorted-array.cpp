ba9a57db50595a7bc85e9fc9822de145f23db417
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-24 19:32:33 +0530

    Time: 47 ms (5.16%), Space: 22.5 MB (78.12%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            int j = i + 1;
+            while(j < nums.size() && nums[j] == nums[i]) {
+                nums.erase(nums.begin() + j);
+            }
+        }
+        return nums.size();
+    }
+};
