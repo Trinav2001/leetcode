ce2bbd8bd672f2b90e3a525f2a84d04112f91c0d
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-05-25 11:00:08 +0530

    Time: 45 ms (77.04%), Space: 28.9 MB (90.33%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> output;
+
+        sort(nums.begin(), nums.end());
+        int sum;
+        int l, r;
+        for(int i = 0; i < nums.size() - 2; i++) {
+
+            if((i > 0) && (nums[i] == nums[i-1])) continue;
+
+            l = i + 1;
+            r = nums.size() - 1;
+
+            while(l < r) {
+                sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0) {
+                    r--;
+                }
+                else if(sum < 0) {
+                    l++;
+                }
+                else {
+                    output.push_back({nums[i], nums[l], nums[r]});
+
+                    while((l < r) && (nums[l] == nums[l+1])) l++;
+                    while((l < r) && (nums[r] == nums[r-1])) r--;
+
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return output;
+    }
+};
