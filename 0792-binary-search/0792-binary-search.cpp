2e71865fa957faace09be6e28e9ff5398360f151
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-01 14:19:40 +0530

    Time: 0 ms (100%), Space: 31.2 MB (79.96%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         while(left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;   // to tackle overflow in mid = (left+right)/2. 2^31 + 2^31 would overflow.
             if(nums[mid] < target) {
                 left = mid + 1;
             }
