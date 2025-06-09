6b2c9168693562ff2fdaec408a13aff3525c8254
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-09 10:50:48 +0530

    Time: 0 ms (100%), Space: 13.3 MB (75.71%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,23 +1,61 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int> arr;
+        // Method - 2
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        int top = 0;
+        int bottom = rows - 1;
 
-        for(auto& row : matrix) {
-            for(auto& val : row) {
-                arr.push_back(val);
+        while(top <= bottom) {
+            int r = (top + bottom) / 2;
+            if(target > matrix[r][columns - 1]) {
+                top = r + 1;
             }
+            else if(target < matrix[r][0]) {
+                bottom = r - 1;
+            }
+            else break;
         }
 
-        int l = 0;
-        int h = arr.size() - 1;
+        if(top > bottom) return false;
 
+        int r = (top + bottom) / 2;
+        int l = 0;
+        int h = columns - 1;
         while(l <= h) {
             int mid = l + (h - l) / 2;
-            if(arr[mid] < target) l = mid + 1;
-            else if(arr[mid] > target) h = mid - 1;
-            else return true;
+
+            if(target > matrix[r][mid]) {
+                l = mid + 1;
+            }
+            else if(target < matrix[r][mid]) {
+                h = mid - 1;
+            }
+            else {
+                return true;
+            }
         }
         return false;
+
+        // Method - 1
+        // vector<int> arr;
+
+        // for(auto& row : matrix) {
+        //     for(auto& val : row) {
+        //         arr.push_back(val);
+        //     }
+        // }
+
+        // int l = 0;
+        // int h = arr.size() - 1;
+
+        // while(l <= h) {
+        //     int mid = l + (h - l) / 2;
+        //     if(arr[mid] < target) l = mid + 1;
+        //     else if(arr[mid] > target) h = mid - 1;
+        //     else return true;
+        // }
+        // return false;
     }
 };
