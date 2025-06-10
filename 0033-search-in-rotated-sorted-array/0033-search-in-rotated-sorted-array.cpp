ade761bc5a4412f4171bafec32e11c42de40ada3
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-10 12:41:42 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,17 +8,16 @@ public:
         while(left <= right) {
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] > nums[left]) {
-                if(target >= nums[left] && target <= nums[mid]) right = mid;
+            if(nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]) right = mid - 1;
                 else left = mid + 1;
             }
 
             else {
-                if(target != nums[left]) left++;
-                else {
-                    res = left;
-                    break;
-                }
+                if(nums[mid] < target && target <= nums[right]) left = mid + 1;
+                else right =  mid - 1;
             }
 
         }
