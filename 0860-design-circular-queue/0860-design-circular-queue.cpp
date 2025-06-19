8af7d84b2a2882f4a4d27fb847008900b5502500
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-19 14:02:32 +0530

    Time: 4 ms (45.28%), Space: 23.6 MB (42.81%) - LeetHub

---
+++ 0860-design-circular-queue/0860-design-circular-queue.cpp
@@ -0,0 +1,69 @@
+class MyCircularQueue {
+
+private:
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode* prev;
+    ListNode(int v, ListNode* n = NULL, ListNode* p = NULL)
+        : val(v), next(n), prev(p) {}
+};
+
+int space;
+ListNode* left;
+ListNode* right;
+
+public:
+    MyCircularQueue(int k) {
+        space = k;
+        left = new ListNode(0);
+        right = new ListNode(0, NULL, left);
+        left -> next = right;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        ListNode* cur = new ListNode(value, right, right -> prev);
+        right -> prev -> next = cur;
+        right -> prev = cur;
+        space--;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        ListNode* temp = left -> next;
+        left -> next = left -> next -> next;
+        left -> next -> prev = left;
+        delete temp;
+        space++;
+        return true;
+    }
+    
+    int Front() {
+        return isEmpty() ? -1 : left -> next -> val;
+    }
+    
+    int Rear() {
+        return isEmpty() ? -1 : right -> prev -> val;
+    }
+    
+    bool isEmpty() {
+        return left -> next == right;
+    }
+    
+    bool isFull() {
+        return space == 0;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
