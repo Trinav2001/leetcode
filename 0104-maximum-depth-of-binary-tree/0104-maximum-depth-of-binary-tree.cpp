7eb72f433376521c7ceac7582695cb9241e883dd
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-22 11:08:58 +0530

    Time: 0 ms (100%), Space: 19.1 MB (13.8%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,27 +13,43 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         // Iterative DFS
-        stack<pair<TreeNode*, int>> stack;
-        stack.push({root, 1});
-        int res = 0;
+        // stack<pair<TreeNode*, int>> stack;
+        // stack.push({root, 1});
+        // int res = 0;
 
-        while(!stack.empty()) {
-            pair<TreeNode*, int> current = stack.top();
-            stack.pop();
-            int depth = current.second;
-            TreeNode* node = current.first;
+        // while(!stack.empty()) {
+        //     pair<TreeNode*, int> current = stack.top();
+        //     stack.pop();
+        //     int depth = current.second;
+        //     TreeNode* node = current.first;
 
-            if(node) {
-                res = max(res, depth);
-                stack.push({node -> left, depth + 1});
-                stack.push({node -> right, depth + 1});
-            }
-        }
-        return res;
+        //     if(node) {
+        //         res = max(res, depth);
+        //         stack.push({node -> left, depth + 1});
+        //         stack.push({node -> right, depth + 1});
+        //     }
+        // }
+        // return res;
 
 
         // BFS
+        queue<TreeNode*> q;
+        if(root != NULL) q.push(root);
 
+        int level = 0;
+
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(node -> left) q.push(node -> left);
+                if(node -> right) q.push(node -> right);
+            }
+            level++;
+        }
+        return level;
         
         // Recursive DFS
         // if(!root) return 0;
