95611ea8d3bd66907b4ec0772f4393ac48594bd8
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-06-24 01:06:54 +0530

    Time: 0 ms (100%), Space: 14.9 MB (62.39%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -16,26 +16,46 @@ public:
 
         if(!root) return res;
 
-        vector<TreeNode*> q;
-        q.push_back(root);
+        queue<TreeNode*> q;
+        q.push(root);
         while(!q.empty()) {
+            TreeNode* backNode = NULL;
             int size = q.size();
-            if(q.back()) {
-                res.push_back(q.back() -> val);
-            
-                for(int i = size; i > 0; i--) {
-                    TreeNode* node = q.front();
-                    q.erase(q.begin());
-                    if(node) {
-                        q.push_back(node -> left);         
-                        q.push_back(node -> right);
-                    }
+
+            for(int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node) {
+                    backNode = node;
+                    q.push(node -> left);
+                    q.push(node -> right);
                 }
             }
-            else {
-                q.pop_back();
+            if(backNode) {
+                res.push_back(backNode -> val);
             }
         }
+
+        // vector<TreeNode*> q;
+        // q.push_back(root);
+        // while(!q.empty()) {
+        //     int size = q.size();
+        //     if(q.back()) {
+        //         res.push_back(q.back() -> val);
+            
+        //         for(int i = size; i > 0; i--) {
+        //             TreeNode* node = q.front();
+        //             q.erase(q.begin());
+        //             if(node) {
+        //                 q.push_back(node -> left);         
+        //                 q.push_back(node -> right);
+        //             }
+        //         }
+        //     }
+        //     else {
+        //         q.pop_back();
+        //     }
+        // }
         return res;
     }
 };
