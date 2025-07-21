c30ae6bc17d2f9a26851b2a5a567afb6cb79d6ea
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-07-21 22:51:07 +0530

    Time: 1 ms (26.16%), Space: 25.9 MB (79.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
-
-        // Method - 2 Time O(n), Space O(1)
-
         int n = height.size();
         int left = 0;
         int right = n - 1;
@@ -36,35 +32,5 @@ public:
             }
         }
         return res;
-    
-        
-        
-
-
-
-        // Method - 1 Time O(n), Space O(n)
-        // int n = height.size();
-        // int res = 0;
-        // vector<int> leftMax(n);
-        // vector<int> rightMax(n);
-
-        // leftMax[0] = height[0];
-        // for(int i = 1; i < n; i++) {
-        //     leftMax[i] = max(leftMax[i - 1], height[i]);
-        // }
-
-        // rightMax[n - 1] = height[n - 1];
-
-        // for(int i = n - 2; i >= 0; i--) {
-        //     rightMax[i] = max(rightMax[i + 1], height[i]);
-        // }
-
-        // for(int i = 1; i < n - 1; i++) {
-        //     res += min(leftMax[i], rightMax[i]) - height[i];
-        // }
-
-
-
-        // return res;
     }
 };
