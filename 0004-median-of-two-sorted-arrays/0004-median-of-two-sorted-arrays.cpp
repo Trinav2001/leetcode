d8efc290326986b488e2b488d19e103d4c2ef5e2
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-08-31 21:07:23 +0530

    Time: 0 ms (100%), Space: 95.3 MB (46.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,9 +4,8 @@ public:
         vector<int>& A = nums1;
         vector<int>& B = nums2;
 
-        int total = (A.size() + B.size());
-        int half = (total) / 2;
-
+        int total = A.size() + B.size();
+        int half = total / 2;
         if(A.size() > B.size()) {
             swap(A, B);
         }
@@ -19,17 +18,15 @@ public:
             int j = half - mid;
 
             int Aleft = (mid > 0) ? A[mid - 1] : INT_MIN;
-            int Aright = (mid < A.size()) ? A[mid] : INT_MAX;
-
+            int Aright = mid < A.size() ? A[mid] : INT_MAX;
             int Bleft = (j > 0) ? B[j - 1] : INT_MIN;
-            int Bright = (j < B.size()) ? B[j] : INT_MAX;
-
+            int Bright = j < B.size() ? B[j] : INT_MAX;
 
             if(Aleft <= Bright && Bleft <= Aright) {
-                
-                if(total % 2) return min(Aright, Bright);
-
-                return ((max(Aleft, Bleft) + min(Aright, Bright)) / 2.0);
+                if(total % 2) {
+                    return min(Aright, Bright);
+                }
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
             }
             else if(Aleft > Bright) {
                 right = mid - 1;
@@ -37,6 +34,8 @@ public:
             else {
                 left = mid + 1;
             }
+
+
         }
         return -1.0;
     }
