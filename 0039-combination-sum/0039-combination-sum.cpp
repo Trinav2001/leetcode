da2969eee36c4bc912bb3b5e9289c2597b0c582f
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-10-07 21:08:34 +0530

    Time: 0 ms (100%), Space: 14.4 MB (41.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> res;
+    vector<vector<int>> result;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> cur;
-        dfs(candidates, 0, 0, cur, target);
-        return res;
+        
+        vector<int> subset;
+        backtrack (subset, 0, 0, target, candidates);
+        return result;
     }
 
-
-    void dfs(vector<int>& candidates, int i, int total, vector<int>& cur, int target) {
-        if(total == target) {
-            res.push_back(cur);
+    void backtrack (vector<int>& subset, int sum, int i, int target, vector<int>& candidates)
+    {
+        if (sum == target) {
+            result.push_back(subset);
             return;
         }
 
-        if(total > target || i >= candidates.size()) {
+        if (sum > target || i >= candidates.size()) {
             return;
         }
 
-        cur.push_back(candidates[i]);
-        dfs(candidates, i, total + candidates[i], cur, target);
+        subset.push_back(candidates[i]);
+        backtrack (subset, sum + candidates[i], i, target, candidates);
 
-        cur.pop_back();
-        dfs(candidates, i + 1, total, cur, target);
+        subset.pop_back();
+        backtrack (subset, sum, i + 1, target, candidates);
     }
-
 };
