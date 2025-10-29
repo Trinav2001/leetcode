6fbf816d031855ef6ef44548a180e82d85197b86
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-10-28 23:41:57 -0600

    Time: 27 ms (57.72%), Space: 16.1 MB (89.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,34 @@
 class Solution {
-    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int ROWS = grid.size(), COLS = grid[0].size();
-        int islands = 0;
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] == '1') {
-                    dfs(grid, r, c);
+int ROWS, COLS;
+vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0,-1}};
+    
+    int numIslands(vector<vector<char>>& grid) {
+    
+        int islands = 0;
+        ROWS = grid.size();
+        COLS = grid[0].size();
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
                     islands++;
                 }
             }
         }
-        
         return islands;
+        
     }
-    
+
     void dfs(vector<vector<char>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.size() || 
-            c >= grid[0].size() || grid[r][c] == '0') {
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS || grid[r][c] == '0') {
             return;
         }
-        
+
         grid[r][c] = '0';
-        for (int i = 0; i < 4; i++) {
+
+        for (int i = 0; i < directions.size(); i++) {
             dfs(grid, r + directions[i][0], c + directions[i][1]);
         }
     }
