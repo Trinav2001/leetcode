91c29d0e088a447f4104464b969ec56b3e8fc798
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-11-07 14:12:49 -0700

    Time: 0 ms (100%), Space: 7.7 MB (86.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> memo(n + 1, -1);
-        return fun(n, memo);
+        // Memoization
+        // vector<int> memo(n + 1, -1);
+        // return fun(n, memo);
+
+        if (n <= 1) return 1;
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int temp = prev1;
+            prev1 = prev1 + prev2;
+            prev2 = temp;
+        }
+
+        return prev1;
         
     }
 
-    int fun (int i, vector<int>& memo) {
-        if (i == 0 || i == 1) {
-            return 1;
-        }
+    // Memoization
+    // int fun (int i, vector<int>& memo) {
+    //     if (i == 0 || i == 1) {
+    //         return 1;
+    //     }
 
-        if (memo[i] != -1) {
-            return memo[i];
-        }
+    //     if (memo[i] != -1) {
+    //         return memo[i];
+    //     }
 
-        memo[i] = fun(i - 1, memo) + fun(i - 2, memo);
-        return memo[i];
-    }
+    //     memo[i] = fun(i - 1, memo) + fun(i - 2, memo);
+    //     return memo[i];
+    // }
 };
