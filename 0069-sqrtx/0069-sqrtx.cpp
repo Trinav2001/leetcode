cf4727f5df1a6764c06307b94c7f34b9b93db4b6
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-11-08 14:56:51 -0700

    Time: 0 ms (100%), Space: 8.4 MB (85.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // Method - 1
-        // return sqrt(x);
 
-        // Method - 2
+        if (x <= 1) {
+            return x;
+        }
+
+        int right = x / 2;
         int left = 0;
-        int res = 0;
-        int right = x;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if((long long)mid * mid > x) {
-                right = mid - 1;
-            }
+        int res = -1;
 
-            else if((long long)mid * mid < x) {
-                res = mid;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if ((long)mid * mid <= x) {
+                res = max (res, mid);
                 left = mid + 1;
             }
+
             else {
-                return mid;
+                right = mid - 1;
             }
         }
+
         return res;
+        
     }
 };
