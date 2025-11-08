de63ef3616d166ece94071df770cc8a4fd4f3310
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-11-08 11:28:06 -0700

    Time: 0 ms (100%), Space: 17.3 MB (96.96%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -4,7 +4,7 @@ public:
         cost.push_back(0);
         int size = cost.size();
         for (int i = size - 3; i >= 0; i--) {
-            cost[i] = min(cost[i] + cost[i + 1], cost[i] + cost[i + 2]);
+            cost[i] += min(cost[i + 1], cost[i + 2]);
         }
 
         return min(cost[0], cost[1]);
