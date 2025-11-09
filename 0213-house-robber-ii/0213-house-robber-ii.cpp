e7d75c692e97396d79a70fb7a713146a059a560a
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-11-08 17:27:20 -0700

    Time: 0 ms (100%), Space: 11.3 MB (8.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
+vector<vector<int>> memo;
     int rob(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
 
-        vector<int> nums1(nums.begin() + 1, nums.end());
-        vector<int> nums2(nums.begin(), nums.end() - 1);
+        memo.resize(nums.size(), vector<int>(2, -1));
+        return max(dfs(nums, 1, 0), dfs(nums, 0, 1));
 
-        int res = max(helper(nums1), helper(nums2));
+        // vector<int> nums1(nums.begin() + 1, nums.end());
+        // vector<int> nums2(nums.begin(), nums.end() - 1);
 
-        return res;
+        // int res = max(helper(nums1), helper(nums2));
+
+        // return res;
         
     }
 
-    int helper (vector<int> & nums) {
-        int rob1 = 0;
-        int rob2 = 0;
-
-        for (auto& num : nums) {
-            int temp = max(num + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+    int dfs (vector<int>& nums, int flag, int i) {
+        if (i >= nums.size() || (flag && i == nums.size() - 1)) {
+            return 0;
         }
 
-        return rob2;
+        if (memo[i][flag] != -1) {
+            return memo[i][flag];
+        }
+
+
+        memo[i][flag] = max(dfs(nums, flag, i + 1), nums[i] + 
+        dfs(nums, flag, i + 2));
+
+        return memo[i][flag];
     }
+
+    // int helper (vector<int> & nums) {
+    //     int rob1 = 0;
+    //     int rob2 = 0;
+
+    //     for (auto& num : nums) {
+    //         int temp = max(num + rob1, rob2);
+    //         rob1 = rob2;
+    //         rob2 = temp;
+    //     }
+
+    //     return rob2;
+    // }
 };
