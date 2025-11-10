984d86b4b2f63f0e459d305a7c75f00234f726f4
Trinav <60963101+Trinav2001@users.noreply.github.com>
2025-11-10 09:22:23 -0700

    Time: 266 ms (5.02%), Space: 65.5 MB (6.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,54 @@
 class Solution {
 public:
+unordered_map<int, int> memo;
     int coinChange(vector<int>& coins, int amount) {
         // Approach 1
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        // vector<int> dp(amount + 1, amount + 1);
+        // dp[0] = 0;
 
-        for (int i = 1; i <= amount; i++) {
-            for (auto& coin : coins) {
-                if (coin <= i) {
-                    int no_of_coin_required = dp[i - coin] + 1;
-                    dp[i] = min(dp[i], no_of_coin_required);
-                }
-            }
+        // for (int i = 1; i <= amount; i++) {
+        //     for (auto& coin : coins) {
+        //         if (coin <= i) {
+        //             int no_of_coin_required = dp[i - coin] + 1;
+        //             dp[i] = min(dp[i], no_of_coin_required);
+        //         }
+        //     }
+        // }
+
+        // return dp[amount] <= amount ? dp[amount] : -1;
+
+        // Approach 2
+        
+        int minCoins = dfs (coins, amount);
+        return minCoins == INT_MAX ? -1 : minCoins;
+        
+    }
+
+
+    int dfs (vector<int>& coins, int amount) {
+        if (amount == 0) {
+            return 0;
         }
 
-        return dp[amount] <= amount ? dp[amount] : -1;
-        
+        if (memo.count(amount)) {
+            return memo[amount];
+        }
+
+        int result = INT_MAX;
+        for (auto& coin : coins) {
+            if (amount - coin >= 0) {
+                int res = dfs(coins, amount - coin);
+                if (res != INT_MAX) {
+                    result = min(result, 1 + res);
+                }
+                
+                
+            }
+            
+        }
+
+        memo[amount] = result;
+        return result;
+
     }
 };
