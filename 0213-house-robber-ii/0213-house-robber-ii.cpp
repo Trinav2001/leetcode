db838a478984537d7f4a548545c351e2ebd29e9e
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-01 07:14:13 -0500

    Time: 0 ms (100%), Space: 10.9 MB (18.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
-vector<vector<int>> memo;
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
+        if (nums.size() <= 1) {
+            return nums[0];
+        }        
 
-        memo.resize(nums.size(), vector<int>(2, -1));
-        return max(dfs(nums, 1, 0), dfs(nums, 0, 1));
+        vector<int> nums1(nums.begin(), nums.end() - 1);
+        vector<int> nums2(nums.begin() + 1, nums.end());
+        int temp1 = helper(nums1);
+        int temp2 = helper(nums2);
 
-        // vector<int> nums1(nums.begin() + 1, nums.end());
-        // vector<int> nums2(nums.begin(), nums.end() - 1);
+        int ans = max(temp1, temp2);
 
-        // int res = max(helper(nums1), helper(nums2));
-
-        // return res;
+        return ans;
         
     }
 
-    int dfs (vector<int>& nums, int flag, int i) {
-        if (i >= nums.size() || (flag && i == nums.size() - 1)) {
-            return 0;
+    int helper(vector<int>& nums) {
+
+        
+
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = max(nums[i] + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
         }
 
-        if (memo[i][flag] != -1) {
-            return memo[i][flag];
-        }
-
-
-        memo[i][flag] = max(dfs(nums, flag, i + 1), nums[i] + 
-        dfs(nums, flag, i + 2));
-
-        return memo[i][flag];
+        return rob2;
     }
-
-    // int helper (vector<int> & nums) {
-    //     int rob1 = 0;
-    //     int rob2 = 0;
-
-    //     for (auto& num : nums) {
-    //         int temp = max(num + rob1, rob2);
-    //         rob1 = rob2;
-    //         rob2 = temp;
-    //     }
-
-    //     return rob2;
-    // }
 };
