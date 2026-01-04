925fdef957f7a2afaf2e5ae707203e3826e30af4
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 05:11:10 -0500

    Time: 0 ms (100%), Space: 14.1 MB (87.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
 
-        vector<int> dp;
+        vector<int> dp;     // increasing (sorted) vector
         dp.push_back(nums[0]);
 
         for (int i = 1; i < nums.size(); i++) {
+
+            // extending if nums[i] is greater than last element (largest) of dp
             if (nums[i] > dp.back()) {
                 dp.push_back(nums[i]);
                 continue;
-            }
+            }   
 
+            /* Finding value in dp that is greater than or equal to nums[i]
+            dp will remain sorted (ascending order) */
             auto it = lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
-            dp[it] = nums[i];
+            dp[it] = nums[i];   // Improving Current Tail
 
 
         }
