3ce9c685904d781dc3d85662a5c3961593539a1d
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 11:19:19 -0500

    Time: 0 ms (100%), Space: 9.4 MB (50.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        dp[1][1] = 1;
 
-        vector<int> row(n, 1);
-        for (int i = 0; i < m - 1; i++) {
-            vector<int> newRow(n, 1);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <=n; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
 
-            for (int j = n - 2; j >= 0; j--) {
-                newRow[j] = newRow[j + 1] + row[j];
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
-
-            row = newRow;
         }
 
-        return row[0];
-        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        // dp[1][1] = 1;
-        // for (int i = 1; i <= m; i++) {
-        //     for (int j = 1; j <= n; j++) {
-        //         if (i == 1 && j == 1) continue;
+        return dp[m][n];
 
-        //         dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        //     }
-        // }
-
-        // return dp[m][n];
+        
     }
 };
