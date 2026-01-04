d4f0f2c2fcdb0feed7510999477d4932eb5a20b3
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 00:24:10 -0500

    Time: 19 ms (78.9%), Space: 18 MB (67.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,63 +1,22 @@
 class Solution {
 public:
-// unordered_map<int, int> memo;
     int coinChange(vector<int>& coins, int amount) {
-
         vector<int> dp(amount + 1, amount + 1);
+
         dp[0] = 0;
 
         for (int i = 1; i < amount + 1; i++) {
             for (auto& coin : coins) {
                 if (coin <= i) {
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
                 }
+
             }
         }
 
         return dp[amount] != amount + 1 ? dp[amount] : -1;
 
-        // Approach 1
-        // vector<int> dp(amount + 1, amount + 1);
-        // dp[0] = 0;
 
-        // for (int i = 1; i <= amount; i++) {
-        //     for (auto& coin : coins) {
-        //         if (coin <= i) {
-        //             int no_of_coin_required = dp[i - coin] + 1;
-        //             dp[i] = min(dp[i], no_of_coin_required);
-        //         }
-        //     }
-        // }
-
-        // return dp[amount] <= amount ? dp[amount] : -1;
-
-        // Approach 2
         
-        // int minCoins = dfs (coins, amount);
-        // return minCoins == INT_MAX ? -1 : minCoins;      
     }
-
-    // int dfs (vector<int>& coins, int amount) {
-    //     if (amount == 0) {
-    //         return 0;
-    //     }
-
-    //     if (memo.count(amount)) {
-    //         return memo[amount];
-    //     }
-
-    //     int result = INT_MAX;
-    //     for (auto& coin : coins) {
-    //         if (amount - coin >= 0) {
-    //             int res = dfs(coins, amount - coin);
-    //             if (res != INT_MAX) {
-    //                 result = min(result, 1 + res);
-    //             }    
-    //         }
-    //     }
-
-    //     memo[amount] = result;
-    //     return result;
-
-    // }
 };
