67d6a83b59bce600ca4532f46ae5fc8413f269f4
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 04:18:56 -0500

    Time: 662 ms (5%), Space: 237.4 MB (5.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,24 +3,32 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        if (sum % 2) return false;
+        if (sum % 2) {
+            return false;
+        }
 
-        int target = sum / 2;   
+        int target = sum / 2;
+
         unordered_set<int> dp;
         dp.insert(0);
 
         for (int i = 0; i < nums.size(); i++) {
             unordered_set<int> temp;
-            for (auto it : dp) {
-                if (it + nums[i] == target) return true;
-                temp.insert(it + nums[i]);
+
+            for (auto& it : dp) {
+
+                if (it + nums[i] == target) {
+                    return true;
+                }
+
                 temp.insert(it);
+                temp.insert(it + nums[i]);
             }
 
             dp = temp;
         }
 
         return false;
-        
+
     }
 };
