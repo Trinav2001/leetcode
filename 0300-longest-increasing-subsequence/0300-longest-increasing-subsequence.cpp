591e3eebe137b56e1ad5328dd749049262a1f40b
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 05:08:35 -0500

    Time: 4 ms (77.13%), Space: 14.2 MB (70.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // Approach 1 - Time O(n^2)
-        // vector<int> LIS(nums.size(), 1);
-        
-        // for (int i = nums.size() - 1; i >= 0; i--) {
-        //     for (int j = i + 1; j < nums.size(); j++) {
-        //         if (nums[i] < nums[j]) {
-        //             LIS[i] = max(LIS[i], 1 + LIS[j]);
-        //         }
-        //     }
-        // }
 
-        // return *max_element(LIS.begin(), LIS.end());  
-
-        // Approach 2 - Time O(nlogn)
         vector<int> dp;
         dp.push_back(nums[0]);
 
@@ -27,8 +14,10 @@ public:
             auto it = lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
             dp[it] = nums[i];
 
+
         }
 
         return dp.size();
+        
     }
 };
