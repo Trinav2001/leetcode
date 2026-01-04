59dc62e527d6ffded9cbb6757e2cfcb0cec07336
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 00:14:22 -0500

    Time: 2 ms (23.74%), Space: 9.2 MB (27.64%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,59 +1,34 @@
 class Solution {
 public:
+unordered_map<int, int> memo;
     int numDecodings(string s) {
 
-        // Approach 1 (Bottom-Up)
-        // vector<int> dp(s.size() + 1);
-        // dp[s.size()] = 1;
-
-        // for (int i = s.size() - 1; i >= 0; i--) {
-        //     if (s[i] == '0') {
-        //         dp[i] = 0;
-        //     }
-        //     else {
-        //         dp[i] = dp[i + 1];
-
-        //         if (i + 1 < s.size() && (s[i] == '1' || 
-        //         (s[i] == '2' && s[i + 1] < '7'))) {
-        //             dp[i] += dp[i + 2];
-        //         }
-        //     }
-        // }
-
-        // return dp[0];
-
-
-        // Approach 2 DP (Top-Down)
-
-
-        unordered_map<int, int> dp;
-        return dfs(s, dp, 0);
+        int res = dfs(s, 0);
+        return res;
         
     }
 
 
-    int dfs (string s, unordered_map<int, int>& dp, int i) {
-
+    int dfs(string& s, int i) {
         if (i == s.size()) {
             return 1;
-        }
-        
-        if (dp.count(i)) {
-            return dp[i];
         }
 
         if (s[i] == '0') {
             return 0;
         }
 
-        int res = dfs(s, dp, i + 1);
-
-        if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] < '7'))) {
-            res += dfs(s, dp, i + 2);
+        if (memo.find(i) != memo.end()) {
+            return memo[i];
         }
 
-        dp[i] = res;
-        return res;
+        int res = dfs(s, i + 1);
 
+        if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] < '7'))) {
+            res += dfs(s, i + 2);
+        }
+
+        memo[i] = res;
+        return res;
     }
 };
