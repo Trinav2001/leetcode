712dad284f9cc11632174c3d35cc2ea975683981
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-04 02:41:05 -0500

    Time: 0 ms (100%), Space: 17.7 MB (59.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-
         int curMax = 1;
         int curMin = 1;
         int res = INT_MIN;
-
         for (auto& num : nums) {
-            int temp = curMax * num;
+            int temp = num * curMax;
             curMax = max(max(num * curMax, num * curMin), num);
-            curMin = min(min(temp, num * curMin), num);
-            // cout<<curMax<<endl<<curMin<<endl;
-
+            curMin = min(min(num * curMin, temp), num);
+            // cout<<curMax<<"    "<<curMin<<endl;
             res = max(res, curMax);
         }
+
         return res;
+        
     }
 };
