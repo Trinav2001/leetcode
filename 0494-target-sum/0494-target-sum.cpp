56cf6607a14e47f1fb1960aa3630f4aba3ff1c8a
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-07 02:43:24 -0500

    Time: 135 ms (36.88%), Space: 53.9 MB (13.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-unordered_map<string, int> memo;
+// unordered_map<string, int> memo;
     int findTargetSumWays(vector<int>& nums, int target) {
-        return dfs(nums, target, 0, 0);
+        // return dfs(nums, target, 0, 0);
+
+        unordered_map<int, int> dp;     // sum -> number of ways(count)
+        dp[0] = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            unordered_map<int, int> nextDp;
+
+            for (auto& pair : dp) {
+                int total = pair.first;
+                int count = pair.second;
+                nextDp[total + nums[i]] += count;
+                nextDp[total - nums[i]] += count;
+            }
+
+            dp = nextDp;
+        }
+
+        return dp[target];
+
+
         
     }
 
-    int dfs(vector<int>& nums, int target, int i, int curSum) {
-        if (i == nums.size()) {
-            return curSum == target;
-        }
+    // int dfs(vector<int>& nums, int target, int i, int curSum) {
+    //     if (i == nums.size()) {
+    //         return curSum == target;
+    //     }
 
-        string key = to_string(i) + "-" + to_string(curSum);
+    //     string key = to_string(i) + "-" + to_string(curSum);
 
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
+    //     if (memo.find(key) != memo.end()) {
+    //         return memo[key];
+    //     }
 
-        memo[key] = dfs(nums, target, i + 1, curSum + nums[i]) +
-                            dfs(nums, target, i + 1, curSum - nums[i]);
+    //     memo[key] = dfs(nums, target, i + 1, curSum + nums[i]) +
+    //                         dfs(nums, target, i + 1, curSum - nums[i]);
 
-        return memo[key];
+    //     return memo[key];
 
 
 
-    }
+    // }
 };
