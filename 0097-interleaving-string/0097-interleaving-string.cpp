e8b1910ed30d4734a04fd16ff7bb0255b4005c06
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-08 06:05:01 -0500

    Time: 0 ms (100%), Space: 9 MB (77.66%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-vector<vector<int>> memo;
     bool isInterleave(string s1, string s2, string s3) {
-        memo.resize(s1.size() + 1, vector<int>(s2.size() + 1, -1));
+
         if (s1.size() + s2.size() != s3.size()) {
             return false;
         }
-        return dfs(0, 0, s1, s2, s3);
+
+        vector<vector<bool>> dp(s1.size() + 1, vector<bool>(s2.size() + 1, false));
+        dp[s1.size()][s2.size()] = true;
+
+        for (int i = s1.size(); i >= 0; i--) {
+            for (int j = s2.size(); j >= 0; j--) {
+                if (i < s1.size() && s1[i] == s3[i + j] && dp[i + 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (j < s2.size() && s2[j] == s3[i + j] && dp[i][j + 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return dp[0][0];
         
-    }
-
-    bool dfs (int i, int j, string& s1, string& s2, string& s3) {
-        if (i == s1.size() && j == s2.size()) {
-            return true;
-        }
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if (i < s1.size() && s1[i] == s3[i + j] && dfs(i + 1, j, s1, s2, s3)) {
-            return true;
-        }
-
-        if (j < s2.size() && s2[j] == s3[i + j] && dfs(i, j + 1, s1, s2, s3)) {
-            return true;
-        }   
-
-
-        memo[i][j] = 0;
-        return false;
     }
 };
