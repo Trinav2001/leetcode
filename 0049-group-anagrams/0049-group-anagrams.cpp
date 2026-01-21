b11ead048401f3db685bf21c024cb7e795fcc370
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-21 10:06:08 -0500

    Time: 179 ms (5.05%), Space: 83.8 MB (5.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // Method - 2
         unordered_map<string, vector<string>> map;
-        for(auto s : strs) {
+
+        for (auto& str : strs) {
             vector<int> count(26, 0);
-            for(auto c : s) {
+            for (auto c : str) {
                 count[c - 'a']++;
             }
+
             string key = "";
-            for(auto i : count) {
+
+            for (auto i : count) {
                 key = key + to_string(i) + ",";
             }
-            map[key].push_back(s);
-        }
-        vector<vector<string>> ans;
-        for(auto& pair : map) {
-            ans.push_back(pair.second);
-        }
-        return ans;
 
+            map[key].push_back(str);
+        }
+
+        vector<vector<string>> ans;
+
+        for (auto& pair : map) {
+            ans.push_back(pair.second);
+
+        }
+
+        return ans;
         
-        // Method - 1
-        // unordered_map<string, vector<string>> map;
-        // for(auto s : strs) {
-        //     string key = s;
-        //     sort(key.begin(), key.end());
-        //     map[key].push_back(s);
-        // }
-        // vector<vector<string>> ans;
-        // for(auto& pair : map) {
-        //     ans.push_back(pair.second);
-        // }
-        // return ans;
     }
 };
