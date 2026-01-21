bb439ba2bc8c2a9cf96dffba1990f220cf147abd
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-21 12:21:35 -0500

    Time: 83 ms (54.31%), Space: 89 MB (23.44%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,69 +1,30 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
 
-        // Method - 3
-        unordered_map<int, int> map;
-        int maxLength = 0;
+        unordered_set<int> set;
         for(auto& num : nums) {
-            if(map[num] == 0) {
-                map[num] = map[num-1] + map[num+1] + 1;
-                map[num - map[num-1]] = map[num];
-                map[num + map[num+1]] = map[num];
-                maxLength = max(maxLength, map[num]);
+            set.insert(num);
+        }
+
+        int maxLength = 0;
+        for (auto& num : set) {
+            if (set.find(num - 1) != set.end()) {
+                continue;
             }
+
+            int curStreak = 1;
+            int curNum = num;
+
+            while (set.find(curNum + 1) != set.end()) {
+                curStreak++;
+                curNum++;
+            }
+
+            maxLength = max(maxLength, curStreak);
         }
 
         return maxLength;
-
-        // Method - 2
-        // unordered_set<int> set;
-        // for(auto& num : nums) {
-        //     set.insert(num);
-        // }
-
-        // int maxLength = 0;
-        // for(auto& num : set) {
-        //     if(set.find(num - 1) != set.end()) {
-        //         continue;
-        //     }
-
-        //     int currentLength = 1;
-        //     int currentNum = num;
-        //     while(set.find(currentNum + 1) != set.end()) {
-        //         currentLength += 1;
-        //         currentNum += 1;
-        //     }
-
-        //     maxLength = max(maxLength, currentLength);
-        // }
-        // return maxLength;
-
-
-
-
-        // Method - 1
-
-        // if(nums.size() == 0) {
-        //     return 0;
-        // }
-        // sort(nums.begin(), nums.end());
         
-        // int currentStreak = 1;
-        // int finalStreak = 1;
-        // for(int i = 1; i < nums.size(); i++) {
-        //     if(nums[i] == nums[i-1]) continue;
-
-        //     if(nums[i] - nums[i-1] == 1) {
-        //         currentStreak += 1;
-        //     }
-
-        //     else {
-        //         currentStreak = 1;
-        //     }
-        //     finalStreak = max(currentStreak, finalStreak);
-        // }
-        // return finalStreak;
     }
 };
