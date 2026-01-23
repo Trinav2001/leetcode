3eb6b46327c6500048b08448928bd56fbbc77ff8
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-23 11:12:39 -0500

    Time: 28 ms (6.81%), Space: 74.5 MB (7.67%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,22 +12,22 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if(preorder.empty() || inorder.empty())  return NULL;
+        
+        if (preorder.empty() || inorder.empty()) return NULL;
 
         TreeNode* root = new TreeNode(preorder[0]);
 
-        auto mid = find(inorder.begin(), inorder.end(), preorder[0]) - 
-                        inorder.begin();
+        auto mid = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin();
 
-        vector<int> leftPre(preorder.begin() + 1, preorder.begin() + mid + 1);
-        vector<int> rightPre(preorder.begin() + mid + 1, preorder.end());
-        vector<int> leftIn(inorder.begin(), inorder.begin() + mid);
-        vector<int> rightIn(inorder.begin() + mid + 1, inorder.end());
+        vector<int> preLeft(preorder.begin() + 1, preorder.begin() + mid + 1);
+        vector<int> preRight(preorder.begin() + mid + 1, preorder.end());
+        vector<int> inLeft(inorder.begin(), inorder.begin() + mid);
+        vector<int> inRight(inorder.begin() + mid + 1, inorder.end());
 
-        root -> left = buildTree(leftPre, leftIn);
-        root -> right = buildTree(rightPre, rightIn);
+        root -> left = buildTree(preLeft, inLeft);
+        root -> right = buildTree(preRight, inRight);
 
         return root;
-
+        
     }
 };
