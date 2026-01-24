f646511bc56d91ed939e6e56ea512428010d5797
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-23 19:46:17 -0500

    Time: 15 ms (28.46%), Space: 26.3 MB (21.83%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+
+vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 int ROWS, COLS;
-int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+
         ROWS = heights.size();
         COLS = heights[0].size();
-        vector<vector<bool>> pacific(ROWS, vector<bool>(COLS, false));
-        vector<vector<bool>> atlantic(ROWS, vector<bool>(COLS, false));
+
+        vector<vector<bool>> pacific(ROWS, vector<bool>(COLS));
+        vector<vector<bool>> atlantic(ROWS, vector<bool>(COLS));
 
         vector<vector<int>> result;
 
@@ -16,19 +20,23 @@ int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for (int i = 0; i < ROWS; i++) {
             pacific[i][0] = true;
             QP.push({i, 0});
+
             atlantic[i][COLS - 1] = true;
             QA.push({i, COLS - 1});
+            
         }
 
-        for (int i = 0; i < COLS; i++) {
-            pacific[0][i] = true;
-            QP.push({0, i});
-            atlantic[ROWS - 1][i] = true;
-            QA.push({ROWS - 1, i});
+        for (int j = 0; j < COLS; j++) {
+            pacific[0][j] = true;
+            QP.push({0, j});
+
+            atlantic[ROWS - 1][j] = true;
+            QA.push({ROWS - 1, j});
+
         }
 
-        pacific = dfs(heights, pacific, QP);
-        atlantic = dfs(heights, atlantic, QA);
+        pacific = dfs (heights, pacific, QP);
+        atlantic = dfs (heights, atlantic, QA);
 
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
@@ -39,31 +47,35 @@ int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         }
 
         return result;
-
-
-
     }
 
-    vector<vector<bool>> dfs(vector<vector<int>>& heights, vector<vector<bool>>& visit, queue<pair<int, int>>& queue) {
+
+    vector<vector<bool>> dfs (vector<vector<int>> heights, vector<vector<bool>> ocean, queue<pair<int, int>> queue) {
 
         while (!queue.empty()) {
             pair<int, int> cur = queue.front();
             queue.pop();
-            
 
             for (int i = 0; i < 4; i++) {
                 int r = cur.first + directions[i][0];
-                int c = cur.second + directions[i][1]; 
-                
+                int c = cur.second + directions[i][1];
+
                 if (r >= 0 && r < ROWS && c >= 0 && c < COLS && 
-                heights[r][c] >= heights[cur.first][cur.second] && 
-                visit[r][c] != true) {
-                    visit[r][c] = true;
-                    queue.push({r, c});
-                }
+                    heights[r][c] >= heights[cur.first][cur.second] &&
+                    ocean[r][c] !=  true) {
+                        ocean[r][c] = true;
+                        queue.push({r, c});
+                    }
             }
         }
 
-        return visit;
+        return ocean;
+
     }
+
+
+
+
+
 };
+
