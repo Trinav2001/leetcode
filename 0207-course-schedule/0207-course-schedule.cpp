d65abf359702a7df268dac6766ffdd8d433c1b78
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-24 11:09:20 -0500

    Time: 1 ms (84.38%), Space: 19.2 MB (82.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        vector<vector<int>> adjacent(n);
-        vector<int> visited(n, 0);
 
-        for (auto& edge : prerequisites) {
-            int u = edge[0];
-            int v = edge[1];
+
+        vector<vector<int>> adjacent(numCourses);
+
+        vector<int> visited(numCourses, 0);
+
+        for (auto& edges : prerequisites) {
+            int u = edges[0];
+            int v = edges[1];
             adjacent[v].push_back(u);
+
         }
 
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0 && !dfs (i, adjacent, visited)) return false;
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && !dfs(i, adjacent, visited))  return false;
         }
+
         return true;
-
         
     }
 
-    bool dfs (int u, vector<vector<int>>& adjacent, vector<int>& visited) {
 
-        if (visited[u] == 1) return false;;
-
-        if (visited[u] == 2) return true;
-
-
-        visited[u] = 1;
-
-        for (auto& edge : adjacent[u]) {
-            if (!dfs (edge, adjacent, visited)) {
-                return 0;
-            }
+    bool dfs (int i, vector<vector<int>>& adjacent, vector<int>& visited) {
+        if (visited[i] == 1) {
+            return false;
         }
-        visited[u] = 2;
 
-        return 1;
+        if (visited[i] == 2) {
+            return true;
+        }
+
+        visited[i] = 1;
+
+        for (auto& edge : adjacent[i]) {
+            if (!dfs(edge, adjacent, visited)) return false;
+        }
+
+        visited[i] = 2;
+
+        return true;
     }
 };
