a32bd4a532100577356608e4f25b695ae33552d3
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-01-24 16:03:48 -0500

    Time: 0 ms (100%), Space: 71.7 MB (51.39%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int curSum = 0;
+        int maxSum = nums[0];
+
+        for (auto& num : nums) {
+            if (curSum < 0) {
+                curSum = 0;
+            }
+
+            curSum += num;
+            maxSum = max(maxSum, curSum);
+        }
+
+        return maxSum;
+        
+    }
+};
