9354878762de3952b63aea501bf3c498451574b2
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-02 16:12:18 -0500

    Time: 0 ms (100%), Space: 19.6 MB (27.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,46 +11,35 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Recursive
+        ListNode* l1 = list1;
+        ListNode* l2 = list2;
 
-        // if(!list1) return list2;
-
-        // if(!list2) return list1;
-
-        // if(list1 -> val <= list2 -> val) {
-        //     list1 -> next = mergeTwoLists(list1 -> next, list2);
-        //     return list1;
-        // }
-        // else {
-        //     list2 -> next = mergeTwoLists(list1, list2 -> next);
-        //     return list2;
-        // }
-
-
-        // Iterative
         ListNode dummy(0);
+
         ListNode* node = &dummy;
-        while(list1 && list2) {
-            
 
-            if(list1 -> val < list2 -> val) {
-                node -> next = list1;
-                list1 = list1 -> next;
+        while (l1 && l2) {
+            if (l1 -> val <= l2 -> val) {
+                node -> next = l1;
+                l1 = l1 -> next;
             }
-            else {
-                node -> next = list2;
-                list2 = list2 -> next;
 
+            else {
+                node -> next = l2;
+                l2 = l2 -> next;
             }
             node = node -> next;
         }
 
-        if(list1) {
-            node -> next = list1;
+        if (l1) {
+            node -> next = l1;   
         }
+
         else {
-            node -> next = list2;
+            node -> next = l2;
         }
+
         return dummy.next;
+
     }
 };
