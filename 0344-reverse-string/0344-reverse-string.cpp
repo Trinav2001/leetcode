baf2fe3f4dbc17669b80154aa4eb943894f5b753
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-11 11:32:24 -0500

    Time: 0 ms (100%), Space: 27.2 MB (80%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+
         int i = 0;
         int j = s.size() - 1;
+
         while (i < j) {
-            swap(s[i], s[j]);
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
             i++;
             j--;
         }
-    }
 
-    void swap(char& a, char& b) {
-        char temp = a;
-        a = b;
-        b = temp;
+        
     }
 };
