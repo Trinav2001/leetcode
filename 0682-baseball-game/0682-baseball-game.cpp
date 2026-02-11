ebbb451a5242ed5af05c109a0f044edbf7bcca17
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-11 10:51:03 -0500

    Time: 6 ms (2.92%), Space: 13 MB (15.84%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
+
         stack<int> stack;
-        int res = 0;
-        for(auto& c : operations) {
-            if(c == "+") {
-                int top = stack.top();
-                stack.pop();
-                int newTop = stack.top() + top;
-                stack.push(top);
-                stack.push(newTop);
-                res += newTop;
-            }
-            else if(c == "D") {
-                int newTop = stack.top() * 2;
-                stack.push(newTop);
-                res += stack.top();
-            }
-            else if(c == "C") {
-                res -= stack.top();
+
+        for (auto& op : operations) {
+            if (op == "C") {
                 stack.pop();
             }
+
+            else if (op == "D") {
+                int a = stack.top();
+                stack.push(a * 2);                
+            }
+
+            else if (op == "+") {
+                int a = stack.top(); stack.pop();
+                int b = stack.top(); stack.pop();
+
+                stack.push(b);
+                stack.push(a);
+                stack.push(a + b);
+            }
+
             else {
-                stack.push(stoi(c));
-                res += stack.top();
+                stack.push(stoi(op));
             }
         }
-        return res;
+
+        int sum = 0;
+
+        while (!stack.empty()) {
+            cout<<stack.top()<< " ";
+            sum = sum + stack.top();
+            stack.pop();
+        }
+
+        return sum;
+
+        
+        
     }
 };
