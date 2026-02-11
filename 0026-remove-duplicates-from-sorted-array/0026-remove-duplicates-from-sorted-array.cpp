06a09f743d9c2fbb99d1c669b356d65c956402cf
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-11 15:40:50 -0500

    Time: 3 ms (12.9%), Space: 22.8 MB (18.36%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-        // Method - 2
-        int l = 1;
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] != nums[i-1]) {
-                nums[l++] = nums[i];
+        int res = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[res] = nums[i];
+                res++;
             }
         }
-        return l;
 
-        // Method - 1
-        // for(int i = 0; i < nums.size(); i++) {
-        //     int j = i + 1;
-        //     while(j < nums.size() && nums[j] == nums[i]) {
-        //         j++;
-        //     }
-        //     nums.erase(nums.begin() + (i + 1), nums.begin() + j);
-        // }
-        // return nums.size();
+        return res;
+        
     }
 };
