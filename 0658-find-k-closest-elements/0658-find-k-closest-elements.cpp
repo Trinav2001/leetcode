626441c1db55082dc88b2cd50b5117f8a1b82919
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-23 18:54:20 -0500

    Time: 3 ms (53.73%), Space: 36.5 MB (52.43%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
         vector<int> output;
-        
         auto it = lower_bound(arr.begin(), arr.end(), x);
-        int idx = it - arr.begin();
-        int right = idx;
-        int left = right - 1;
-
-        int window = right - left - 1;
+        int index = it - arr.begin();
         
-        while(window < k) {
-            if (left < 0) right++;
-            else if (right >= arr.size()) left--;
-            else {
-                if (abs(x - arr[left]) <= abs(x - arr[right])) left--;
-                else right++;
+        int left, right;
+        left = index;
+        right = index;
+
+        if (index == arr.size()) {
+            left--;
+            right--;
+        }
+
+        else if (index > 0 && abs(x - arr[index - 1]) <= abs(x - arr[index])) {
+            left--;
+            right--;
+        }
+        
+      
+
+        int window = right - left + 1;
+
+        while (window < k) {
+            if (left <= 0) {
+                right++;
             }
-            window = right - left - 1;
+            else if (right >= arr.size() - 1) {
+                left--;
+            }
+
+            else {
+                if (abs(x - arr[left - 1]) <= abs(x - arr[right + 1])) {
+                    left--;
+                }
+
+                else {
+                    right++;
+                }
+            }
+
+            window = right - left + 1;
         }
 
-        for(int i = left + 1; i < right; i++) {
+        for (int i = left; i <= right; i++) {
             output.push_back(arr[i]);
+
         }
 
-
-        
         return output;
+        
     }
 };
