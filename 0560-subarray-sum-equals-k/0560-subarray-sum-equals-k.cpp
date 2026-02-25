2c1f0200ef8d1096bcd0724125381c1c4a2ff220
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-24 19:52:59 -0500

    Time: 35 ms (86.66%), Space: 45.4 MB (68.66%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixSum;
-        int currentSum = 0;
+
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int prefixSum = 0;
         int diff;
+        
         int output = 0;
-        prefixSum[0] = 1;
-        for(auto& num : nums) {
-            currentSum += num;
-            diff = currentSum - k;
-            output += prefixSum[diff];
-            prefixSum[currentSum]++;
+
+        for (auto& num: nums) {
+            prefixSum += num;
+            diff = prefixSum - k;
+
+            if (map.find(diff) != map.end()) {
+                output += map[diff];
+            }
+
+            map[prefixSum] += 1;
+
         }
 
         return output;
+        
     }
 };
