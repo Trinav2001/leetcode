e5efa2c742a5ccc536985f4256d60ceee5a327bd
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-25 11:13:19 -0500

    Time: 4 ms (19.87%), Space: 28.2 MB (27.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Boyer-Moore Voting Algo
-        // Keep a count and a candidate
-        // If the current number equals the candidate → count++
-        // If different → count--
-        // When count drops to 0 → choose the new candidate
-        int count = 0;
-        int candidate;
-        for(auto val : nums) {
-            if(count == 0) {
-                candidate = val;
-                count++;
-            }
-            else {
-                if(candidate == val) {
-                    count++;
-                }
-                else {
-                    count--;
-                }
+
+        unordered_map<int, int> map;
+
+        int n = nums.size();
+        int bound = n / 2;
+        
+
+        for (auto& num : nums) {
+            map[num]++;
+            
+            if (map[num] > bound) {
+                return num;
             }
         }
-        return candidate;
 
-
-        // Method - 1
-        // unordered_map<int, int> map;
-        // for(auto key : nums) {
-        //     map[key] += 1;
-        // }
-        // int ans = nums[0];
-        // int freq = -1;
-        // for(auto val : map) {
-        //     if(val.second > freq) {
-        //         freq = val.second;
-        //         ans = val.first;
-        //     }
-        // }
-        // return ans;
+        return 0;
+        
     }
 };
