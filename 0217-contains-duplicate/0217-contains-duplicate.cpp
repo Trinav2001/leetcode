ce7aa7e7282a74f506b3af70ac224971d8948956
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-26 13:05:13 -0500

    Time: 38 ms (72.92%), Space: 89.4 MB (72.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,6 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
 
         unordered_set<int> set;
+        set.reserve(nums.size());
 
         for (auto& num : nums) {
             auto pair = set.insert(num);
