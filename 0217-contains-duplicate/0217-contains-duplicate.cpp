53115cc132e4c5fcaa23eeff96bcc9417293c760
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-26 13:04:08 -0500

    Time: 42 ms (72.14%), Space: 90.9 MB (44.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        // Method 3
-        s.reserve(nums.size());   // avoiding rehashing
-        for(auto val : nums) {
-            if(!s.insert(val).second) {
+
+        unordered_set<int> set;
+
+        for (auto& num : nums) {
+            auto pair = set.insert(num);
+            if (!pair.second) {
                 return true;
             }
         }
+
         return false;
-
-
-        // Method 2
-        // unordered_map<int, int> m;
-        // m.reserve(nums.size());
-        // for(auto val : nums) {
-        //     m[val]++;
-        //     if(m[val] > 1) {
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-
-        // Method 1
-        // for(auto val : nums) {
-        //     s.insert(val);
-        // }
-        // if(s.size() == nums.size()) {
-        //     return false;
-        // }
-        // return true;
+        
     }
 };
