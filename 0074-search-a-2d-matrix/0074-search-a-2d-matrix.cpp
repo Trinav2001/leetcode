2ffb6390cc39c140791a423ee43a681a6b5d9bf0
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-02-27 15:38:27 -0500

    Time: 0 ms (100%), Space: 13.3 MB (77.96%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,48 +1,61 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
+        // Method - 2
         int rows = matrix.size();
-        int cols = matrix[0].size();
+        int columns = matrix[0].size();
+        int top = 0;
+        int bottom = rows - 1;
 
-        int start = 0;
-        int end = rows - 1;
-
-        while (start <= end) {
-            int midRow = start + (end - start) / 2;
-            if (target < matrix[midRow][0]) {
-                end = midRow - 1;
+        while(top <= bottom) {
+            int r = (top + bottom) / 2;
+            if(target > matrix[r][columns - 1]) {
+                top = r + 1;
             }
-
-            else if (target > matrix[midRow][cols - 1]) {
-                start = midRow + 1;
+            else if(target < matrix[r][0]) {
+                bottom = r - 1;
             }
-
-            else {
-                int left = 0;
-                int right = cols - 1;
-
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-
-                    if (target < matrix[midRow][mid]) {
-                        right = mid - 1;
-                    }
-
-                    else if (target > matrix[midRow][mid]) {
-                        left = mid + 1;
-                    }
-                    else {
-                        return true;
-                   }
-                }
-
-                return false;
-            }
+            else break;
         }
 
+        if(top > bottom) return false;
+
+        int r = (top + bottom) / 2;
+        int l = 0;
+        int h = columns - 1;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if(target > matrix[r][mid]) {
+                l = mid + 1;
+            }
+            else if(target < matrix[r][mid]) {
+                h = mid - 1;
+            }
+            else {
+                return true;
+            }
+        }
         return false;
 
-        
+        // Method - 1
+        // vector<int> arr;
+
+        // for(auto& row : matrix) {
+        //     for(auto& val : row) {
+        //         arr.push_back(val);
+        //     }
+        // }
+
+        // int l = 0;
+        // int h = arr.size() - 1;
+
+        // while(l <= h) {
+        //     int mid = l + (h - l) / 2;
+        //     if(arr[mid] < target) l = mid + 1;
+        //     else if(arr[mid] > target) h = mid - 1;
+        //     else return true;
+        // }
+        // return false;
     }
 };
