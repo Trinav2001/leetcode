493c345e57e71f481b2fb7b1df804ded57a86ce3
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-02 21:23:23 -0500

    Time: 12 ms (8.1%), Space: 30 MB (5.28%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while(i < j) {
-            if(s[i] != s[j]) {
-                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+
+        return isPalindrome(s, 0, s.length() - 1);
+
+        
+    }
+
+    bool isPalindrome(string s, int left, int right) {
+        while (left <= right) {
+            if (s[left] != s[right]) {
+                return helper(s, left + 1, right) || helper(s, left, right - 1);
             }
-            i++;
-            j--;
+            left++;
+            right--;
         }
+
         return true;
     }
 
-    bool isPalindrome(string s, int i, int j) {
-        while(i < j) {
-            if(s[i] != s[j]) {
+    bool helper(string s, int l, int r) {
+        while (l <= r) {
+            if (s[l] != s[r]) {
                 return false;
             }
-            i++;
-            j--;
+
+            l++;
+            r--;
         }
+
         return true;
     }
 };
