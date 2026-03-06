9e1023026a4783fb72a27d7385ad5ca7a512011b
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-05 19:03:45 -0500

    Time: 0 ms (100%), Space: 9.6 MB (96.55%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,41 +1,46 @@
 class MyQueue {
-
-private:
-stack<int> s1, s2;
-
 public:
+stack<int> s1;
+stack<int> s2;
     MyQueue() {
         
     }
     
     void push(int x) {
+
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
         s1.push(x);
+
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+
+        
     }
     
     int pop() {
-        if(s2.empty()) {
-            while(!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        int val = s2.top();
-        s2.pop();
-        return val;
+        int x = s1.top();
+        s1.pop();
+        return x;
     }
     
     int peek() {
-        if(s2.empty()) {
-            while(!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        return s2.top();
+        return s1.top();
+        
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
+        if (s1.empty()) {
+            return true;
+        }
+
+        return false;
+        
     }
 };
 
