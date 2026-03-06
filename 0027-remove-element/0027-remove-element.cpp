2690de40df807e62ca3c2aeeca50abbe3ac5b5da
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-05 19:30:27 -0500

    Time: 0 ms (100%), Space: 11.6 MB (83.18%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-        // Method - 2
-        int i = 0;
-        for(int j = 0; j < nums.size(); j++) {
-            if(nums[j] != val) {
-                nums[i++] = nums[j];
+
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[idx++] = nums[i];
             }
         }
-        return i;
+
+        return idx;
         
-        // Method - 1
-        // for(int i = 0; i < nums.size(); i++) {
-        //     if(nums[i] == val) {
-        //         nums.erase(nums.begin() + i);
-        //         --i;
-        //     }
-        // }
-        // return nums.size();
     }
 };
