e36978f8759d02d8f93f67dda174e0ea1eacd82a
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-07 20:41:18 -0500

    Time: 0 ms (100%), Space: 9.4 MB (51.67%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
     string decodeString(string s) {
+
         vector<string> stack;
 
-        for(char&c : s) {
-            if(c != ']') stack.push_back(string(1, c));
+        for (auto& c : s) {
+            if (c != ']') {
+                stack.push_back(string(1, c));  // char to string of size = 1
+            }
 
             else {
+                
                 string substr = "";
                 while(stack.back() != "[") {
                     substr = stack.back() + substr;
                     stack.pop_back();
                 }
+
                 stack.pop_back();
 
-                string number = "";
-                while(!stack.empty() && isdigit(stack.back()[0])) {
-                    number = stack.back() + number;
+                string n = "";
+
+                string k = "";
+
+                while (!stack.empty() && isdigit(stack.back()[0])) {
+                    k = stack.back() + k;
                     stack.pop_back();
                 }
 
-                int count = stoi(number);
+                int count = stoi(k);
 
                 string repeat = "";
-                for(int i = 0; i < count; i++) {
+                for (int i = 0; i < count; i++) {
                     repeat += substr;
                 }
+
                 stack.push_back(repeat);
             }
         }
 
-
         string res = "";
 
-        for(auto& part : stack) {
-            res += part;
+        for (auto& s : stack) {
+            res += s;
         }
+
         return res;
 
+
+
+
+        
     }
 };
