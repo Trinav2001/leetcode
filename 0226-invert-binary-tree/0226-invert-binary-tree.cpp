43cde37a06ed12a3882018ae78aa35924ffd8c44
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-10 19:23:55 -0400

    Time: 0 ms (100%), Space: 12.6 MB (25.57%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,12 +12,15 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root) return NULL;
-        
+
+        if (!root) return NULL;
+
         swap(root -> left, root -> right);
+
         invertTree(root -> left);
         invertTree(root -> right);
 
         return root;
+        
     }
 };
