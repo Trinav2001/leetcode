54d07bbf2e48cab0726ee8665048d8361a032c59
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-10 15:31:52 -0400

    Time: 0 ms (100%), Space: 11 MB (73.3%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,35 +12,73 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-
-        stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
+        // Method - 2
+        vector<bool> visited;
         vector<int> res;
-
-        if (!root) {
-            return res;
-        }
-
-        st1.push(root);
         TreeNode* cur;
+        stack<TreeNode*> st;
 
-        while (!st1.empty()) {
-            cur = st1.top();
-            st1.pop();
+        st.push(root);
+        visited.push_back(false);
+        
+        while (!st.empty()) {
+            cur = st.top();
+            st.pop();
+            bool v = visited.back();
+            visited.pop_back();
+
+            if (cur) {
+                if (v) {
+                    res.push_back(cur -> val);
+                }
+
+                else {
+                    
+                    st.push(cur);
+                    visited.push_back(true);
+
+                    st.push(cur -> right);
+                    visited.push_back(false);
+
+                    st.push(cur -> left);
+                    visited.push_back(false);
+                }
+            }
             
-            st2.push(cur);
-
-            if (cur -> left) st1.push(cur -> left);
-            if (cur -> right) st1.push(cur -> right);
-            
-        }
-
-        while (!st2.empty()) {
-            res.push_back(st2.top() -> val) ;
-            st2.pop();
         }
 
         return res;
+        
+        
+        // Method - 1
+        // stack<TreeNode*> st1;
+        // stack<TreeNode*> st2;
+        // vector<int> res;
+
+        // if (!root) {
+        //     return res;
+        // }
+
+        // st1.push(root);
+        // TreeNode* cur;
+
+        // while (!st1.empty()) {
+        //     cur = st1.top();
+        //     st1.pop();
+            
+        //     st2.push(cur);
+
+        //     if (cur -> left) st1.push(cur -> left);
+        //     if (cur -> right) st1.push(cur -> right);
+            
+        // }
+
+        // while (!st2.empty()) {
+        //     res.push_back(st2.top() -> val) ;
+        //     st2.pop();
+        // }
+
+        // return res;
         
     }
 };
