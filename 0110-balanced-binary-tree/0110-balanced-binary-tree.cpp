903b0d9dca060336c6fee6f7246da7744d341631
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-10 20:18:43 -0400

    Time: 0 ms (100%), Space: 23.2 MB (20.43%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,20 +12,24 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
+
         pair<bool, int> res = DFS(root);
         return res.first;
+        
     }
 
-private:
-    pair<bool, int> DFS(TreeNode* root) {
-        if(!root) return {true, 0};
+    pair<bool, int> DFS(TreeNode* node) {
 
-        pair<bool, int> left = DFS(root -> left);
-        pair<bool, int> right = DFS(root -> right);
+        if (!node) {
+            return {true, 0};
+        }
+
+        pair<bool, int> left = DFS(node -> left);
+        pair<bool, int> right = DFS(node -> right);
 
         bool balanced = left.first && right.first && 
-                        abs(left.second - right.second) <= 1;
-        
+                    abs(left.second - right.second) <= 1;
+
         return {balanced, 1 + max(left.second, right.second)};
 
     }
