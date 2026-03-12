e9ac8c8f7e1540f05174144d42b9cb3bbc8f7a0d
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-12 08:19:24 -0400

    Time: 0 ms (100%), Space: 17.8 MB (11.58%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,13 +1,29 @@
 class Solution {
+
+private:
+vector<int> memo;
+
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int size = cost.size();
 
-        for (int i = size - 3; i >= 0; i--) {
-            cost[i] += min(cost[i + 1],  cost[i + 2]);
-        }
+        int n = cost.size();
+        memo.resize(n + 1, -1);
 
-        return min(cost[0], cost[1]);
+        return min(dp(n - 1, cost), dp(n - 2, cost));
         
     }
-};
+
+    int dp(int i, vector<int>& cost) {
+        
+        if (i == 0 || i == 1) {
+            return cost[i];
+        }
+
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+
+        memo[i] = cost[i] + min(dp(i - 1, cost), dp(i - 2, cost));
+        return memo[i];
+    }
+}; 
