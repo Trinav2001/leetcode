e495f5cd4867675a2f92ab6a4c4999a369790b7e
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-12 09:23:09 -0400

    Time: 0 ms (100%), Space: 11 MB (18.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() <= 1) {
+
+        if (nums.size() == 1) {
             return nums[0];
-        }        
+        }
 
         vector<int> nums1(nums.begin(), nums.end() - 1);
         vector<int> nums2(nums.begin() + 1, nums.end());
-        int temp1 = helper(nums1);
-        int temp2 = helper(nums2);
 
-        int ans = max(temp1, temp2);
+        int first = dp(nums1);
+        int second = dp(nums2);
 
-        return ans;
+        return max(first, second);
         
     }
 
-    int helper(vector<int>& nums) {
+    int dp(vector<int>& nums) {
 
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
         
+        
+        int rob2 = nums[0];
+        int rob1 = max(nums[1], rob2);
 
-        int rob1 = 0;
-        int rob2 = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int temp = max(nums[i] + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+        for (int i = 2; i < n; i++) {
+            int temp = max(nums[i] + rob2, rob1);
+            rob2 = rob1;
+            rob1 = temp;
         }
 
-        return rob2;
+        return max(rob1, rob2);
+
+        
+        
     }
 };
