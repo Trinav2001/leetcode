d8c86ad45f7008824c1ae3db24dc24dfd67bcfab
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-24 01:02:02 -0400

    Time: 0 ms (100%), Space: 14.3 MB (26.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
+private:
+    vector<vector<int>> res;
 public:
-    vector<vector<int>> result;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<int> subset;
-        backtrack (candidates, subset, 0, 0, target);
-        return result;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, 0, 0, subset);
+
+        return res;
     }
 
-    void backtrack (vector<int>& candidates, vector<int>& subset, int sum, int i, int target) {
+    void backtrack(vector<int>& candidates, int target, int sum, int i, vector<int>& subset) {
         if (sum == target) {
-            result.push_back(subset);
+            res.push_back(subset);
             return;
         }
 
@@ -19,12 +21,12 @@ public:
         }
 
         subset.push_back(candidates[i]);
-        backtrack (candidates, subset, sum + candidates[i], i + 1, target);
+        backtrack(candidates, target, candidates[i] + sum, i + 1, subset);
 
         subset.pop_back();
         while (i + 1 < candidates.size() && candidates[i] == candidates[i + 1]) {
             i++;
         }
-        backtrack (candidates, subset, sum, i + 1, target);
+        backtrack(candidates, target, sum, i + 1, subset);
     }
 };
