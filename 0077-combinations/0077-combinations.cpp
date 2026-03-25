b3b1488317508917f54029fa45997e9635b7e05b
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-25 16:49:32 -0400

    Time: 105 ms (17%), Space: 131.3 MB (13.31%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,45 +1,31 @@
 class Solution {
-public:
+private:
     vector<vector<int>> res;
+public:
     vector<vector<int>> combine(int n, int k) {
-        vector<int> combo;
-        // dfs(n, k, 1, combo);
-        backtrack(1, combo, k, n);
+        vector<int> subset;
+        backtrack(n, k, subset, 1);
         return res;
-
+        
     }
 
-    // Backtracking
-    void backtrack(int start, vector<int>&  combo, int k, int n) {
-        if(combo.size() == k) {
-            res.push_back(combo);
+    void backtrack(int n, int k, vector<int>& subset, int i) {
+        
+        if (subset.size() == k && subset.back() <= n) {
+            res.push_back(subset);
             return;
         }
 
-        for(int i = start; i < n + 1; i++) {
-            combo.push_back(i);
-            backtrack(i + 1, combo, k, n);
-            combo.pop_back();
+        if (i > n) {
+            return;
         }
+
+        
+
+        subset.push_back(i);
+        backtrack(n, k, subset, i + 1);
+
+        subset.pop_back();
+        backtrack(n, k, subset, i + 1);
     }
-
-    // void dfs(int n, int k, int i, vector<int>& combo) {
-    //     if(i > n) {
-    //         if(combo.size() == k) {
-    //             res.push_back(combo);
-    //         }
-    //         return;
-    //     }
-
-    //     combo.push_back(i);
-    //     dfs(n, k, i + 1, combo);
-
-    //     combo.pop_back();
-    //     dfs(n, k, i + 1, combo);
-
-    // }
-
-
-
-
 };
