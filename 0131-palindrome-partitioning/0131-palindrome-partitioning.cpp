931bfcc88d662a0395f95c284b922b2b9d3269ed
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-03-31 00:31:17 -0400

    Time: 88 ms (15.85%), Space: 88 MB (16.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,38 @@
 class Solution {
+vector<vector<string>> res;
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> partition;
-
-        backtrack(0, s, partition, result);
-        return result;
+        vector<string> palindrome;
+        backtrack(0, s, palindrome);
+        return res;        
     }
-private:
-    void backtrack(int start, string s, vector<string>& partition, vector<vector<string>>& result) {
-        if (start >= s.length()) {
-            result.push_back(partition);
+
+    void backtrack(int start, string s, vector<string>& palindrome) {
+        if (start == s.length()) {
+            res.push_back(palindrome);
             return;
         }
 
+
         for (int end = start; end < s.length(); end++) {
             if (isPalindrome(s, start, end)) {
-                partition.push_back(s.substr(start, end - start + 1));
-                backtrack(end + 1, s, partition, result);
-                partition.pop_back();
-
+                palindrome.push_back(s.substr(start, end - start + 1));
+                backtrack(end + 1, s, palindrome);
+                palindrome.pop_back();
             }
         }
+
     }
 
-    bool isPalindrome(string& s, int l, int r) {
-        while (l < r) {
-            if (s[l] != s[r]) return false;
-            l++;
-            r--;
+    bool isPalindrome(string s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
         }
+
         return true;
     }
 };
