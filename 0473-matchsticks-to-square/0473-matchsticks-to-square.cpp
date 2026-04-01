850c9added5e1136fae3e82238d06bc541738131
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-01 13:39:49 -0400

    Time: 251 ms (20.69%), Space: 12.9 MB (28.04%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -2,27 +2,35 @@ class Solution {
 public:
     bool makesquare(vector<int>& matchsticks) {
         int sum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
-        if(sum % 4 != 0) return false;
+        if (sum % 4) {
+            return false;
+        }
 
+        int targetSide = sum / 4;
         sort(matchsticks.rbegin(), matchsticks.rend());
+
         vector<int> sides(4, 0);
-        return backtrack(matchsticks, 0, sides, sum/4);
+
+        return backtrack(matchsticks, sides, 0, targetSide);
         
     }
 
-    bool backtrack(vector<int>& matchsticks, int i, vector<int>& sides, int targetLength) {
-        if(i == matchsticks.size()) {
+    bool backtrack(vector<int>& matchsticks, vector<int>& side, int i, int targetSide) {
+        if (i == matchsticks.size()) {
             return true;
         }
 
-        for(int j = 0; j < 4; j++) {
-            if(sides[j] + matchsticks[i] <= targetLength) {
-                sides[j] += matchsticks[i];
-                if (backtrack(matchsticks, i + 1, sides, targetLength)) return true;
-                sides[j] -= matchsticks[i];
+        for (int k = 0; k < 4; k++) {
+            if (side[k] + matchsticks[i] <= targetSide) {
+                side[k] += matchsticks[i];
+                if (backtrack(matchsticks, side, i + 1, targetSide)) {
+                    return true;
+                }
+                side[k] -= matchsticks[i];
+
             }
         }
+
         return false;
     }
-
 };
