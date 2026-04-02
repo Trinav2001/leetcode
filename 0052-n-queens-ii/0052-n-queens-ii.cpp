b88e849318cd6083385acecb2c07e155b82ce6da
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-02 12:55:27 -0400

    Time: 8 ms (9.41%), Space: 9.6 MB (10.74%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
-
-unordered_set<int> col;
-unordered_set<int> posDiag;
-unordered_set<int> negDiag;
-int res;
-
-
+private:
+    unordered_set<int> cols;
+    unordered_set<int> negDiag;
+    unordered_set<int> posDiag;
+    int res;
 public:
     int totalNQueens(int n) {
-
-        backtrack(0, n);
+        backtrack(n, 0);
         return res;
+        
     }
 
-    void backtrack(int r, int n) {
-        if(r == n) {
+    void backtrack(int n, int r) {
+        if (r == n) {
             res++;
             return;
         }
 
-        for(int c = 0; c < n; c++) {
-            if(col.count(c) || posDiag.count(r + c) || negDiag.count(r - c)) continue;
+        for (int c = 0; c < n; c++) {
+            if (cols.count(c) || negDiag.count(r - c) || posDiag.count(r + c)) {
+                continue;
+            }
 
-            col.insert(c);
-            posDiag.insert(r + c);
+            cols.insert(c);
             negDiag.insert(r - c);
+            posDiag.insert(r + c);
 
-            backtrack(r + 1, n);
-            col.erase(c);
-            posDiag.erase(r + c);
+            backtrack(n, r + 1);
+
+            cols.erase(c);
             negDiag.erase(r - c);
+            posDiag.erase(r + c);
+
         }
+
+
     }
 };
