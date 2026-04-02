b0d122a36ed8f4ce64ef711ecc975a4b762c4481
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-02 12:49:46 -0400

    Time: 7 ms (17.24%), Space: 12.4 MB (14.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,36 +1,42 @@
 class Solution {
-public:
-unordered_set<int> col;
-unordered_set<int> negDiag;
-unordered_set<int> posDiag;
-vector<vector<string>> res;
+private:
+    vector<vector<string>> res;
+    unordered_set<int> cols;
+    unordered_set<int> negDiag;
+    unordered_set<int> posDiag;
 
+public:
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n, string(n, '.'));
-
-        backtrack(0, n, board);
+        backtrack(n, board, 0);
+        
         return res;
     }
 
-    void backtrack(int r, int n, vector<string>& board) {
-        if(r == n) {
+    void backtrack(int n, vector<string>& board, int r) {
+        if (r == n) {
             res.push_back(board);
             return;
         }
 
-        for(int c = 0; c < n; c++) {
-            if(col.count(c) || posDiag.count(r + c) || negDiag.count(r - c)) continue;
+        for (int c = 0; c < n; c++) {
+            if (cols.count(c) || negDiag.count(r - c) || posDiag.count(r + c)) {
+                continue;
+            }
 
-            col.insert(c);
-            posDiag.insert(r + c);
-            negDiag.insert(r - c);
             board[r][c] = 'Q';
+            cols.insert(c);
+            negDiag.insert(r - c);
+            posDiag.insert(r + c);
 
-            backtrack(r + 1, n, board);
-            col.erase(c);
-            posDiag.erase(r + c);
+            backtrack(n, board, r + 1);
+
+            cols.erase(c);
             negDiag.erase(r - c);
+            posDiag.erase(r + c);
             board[r][c] = '.';
         }
     }
+
+    
 };
