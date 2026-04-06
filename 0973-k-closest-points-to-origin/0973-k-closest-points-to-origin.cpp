17f54405b3414ae2e817ce985a792c2a93f3ab34
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-05 23:04:39 -0400

    Time: 571 ms (5.01%), Space: 185.5 MB (5.01%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
-        auto comp = [](vector<int>& a, vector<int>& b) {
-            return a[0] * a[0] + a[1] * a[1] > b[0] * b[0] + b[1] * b[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> minHeap;
+        auto comp = [](vector<int> a, vector<int> b) {
+            long long distA = a[0] * a[0] + a[1] * a[1];
+            long long distB = b[0] * b[0] + b[1] * b[1];
 
+            return distA > distB;
+        };
+
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> minHeap;
         vector<vector<int>> res;
 
-        for (auto& pt : points) {
-            minHeap.push(pt);
+        for (auto& pts : points) {
+            minHeap.push(pts);
         }
+
 
         while (k--) {
             res.push_back(minHeap.top());
             minHeap.pop();
+
         }
 
         return res;
