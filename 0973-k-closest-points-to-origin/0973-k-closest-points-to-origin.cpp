52463281c135861f0dadf88b9d863a05515a84b6
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-05 23:07:40 -0400

    Time: 565 ms (5.01%), Space: 185.6 MB (5.01%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-
+        // For priority_queue, comparator defines when one element should be 
+        // considered lower priority than another. 
+        
+        // a has lower priority than b if dist(a) > dist(b)
         auto comp = [](vector<int> a, vector<int> b) {
             long long distA = a[0] * a[0] + a[1] * a[1];
             long long distB = b[0] * b[0] + b[1] * b[1];
