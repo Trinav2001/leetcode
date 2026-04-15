d96344e441748cbbfcd9c99f6c410d89a54a9951
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-15 01:04:13 -0400

    Time: 0 ms (100%), Space: 87.2 MB (75.78%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -5,10 +5,6 @@ public:
         int five = 0, ten = 0;
 
         for (auto& b : bills) {
-            if (b > 5 && five == 0) {
-                return false;
-            }
-
             if (b == 5) {
                 five++;
             }
@@ -27,12 +23,11 @@ public:
                 else {
                     five -= 3;
                 }
-                
-                if (five < 0) {
-                    return false;
-                }
             }
-            cout<<five<< " "<<ten<< endl;
+
+            if (five < 0) {
+                return false;
+            }
         }
 
         return true;
