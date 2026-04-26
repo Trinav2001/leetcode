05916c990be935f58e95e5216edf4725bc6190b6
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-04-26 00:18:03 -0700

    Time: 114 ms (13.75%), Space: 131.3 MB (14.06%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -4,14 +4,13 @@ private:
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<int> subset;
-        backtrack(n, k, subset, 1);
+        backtrack(n, k, 1, subset);
         return res;
         
     }
 
-    void backtrack(int n, int k, vector<int>& subset, int i) {
-        
-        if (subset.size() == k && subset.back() <= n) {
+    void backtrack(int n, int k, int i, vector<int>& subset) {
+        if (subset.size() == k) {
             res.push_back(subset);
             return;
         }
@@ -20,12 +19,10 @@ public:
             return;
         }
 
-        
-
         subset.push_back(i);
-        backtrack(n, k, subset, i + 1);
+        backtrack(n, k, i + 1, subset);
 
         subset.pop_back();
-        backtrack(n, k, subset, i + 1);
+        backtrack(n, k, i + 1, subset);
     }
 };
