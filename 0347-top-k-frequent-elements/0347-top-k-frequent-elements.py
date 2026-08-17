65a68e98f36dd5f704f46208af220e4ef8ac6e79
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-08-17 12:22:31 -0400

    Time: 16 ms (6.01%), Space: 24.8 MB (20.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,22 +2,42 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
+        # Priority Queue
+        # count = {}
 
+        # for num in nums:
+        #     count[num] = count.get(num, 0) + 1
+
+        # heap = []
+
+        # for key in count:
+        #     heapq.heappush(heap, (-count[key], key))
+
+        # ans = []
+
+        # while k > 0:
+        #     ans.append(heapq.heappop(heap)[1])
+        #     k -= 1
+        
+        # return ans
+
+
+        # Bucket Sort
+        count = {}
         for num in nums:
             count[num] = count.get(num, 0) + 1
-
-        heap = []
-
-        for key in count:
-            heapq.heappush(heap, (-count[key], key))
-
-        ans = []
-
-        while k > 0:
-            ans.append(heapq.heappop(heap)[1])
-            k -= 1
         
-        return ans
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        for key, freq in count.items():
+            bucket[freq].append(key)
+
+        ans = []    
+        for freq in range(len(bucket) - 1, 0, -1):
+            for num in bucket[freq]:
+                ans.append(num)
+
+                if len(ans) == k:
+                    return ans
 
         
