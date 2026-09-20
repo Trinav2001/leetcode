24af4f98534ec440298ec532bc47062ddbdc70bf
Trinav <60963101+Trinav2001@users.noreply.github.com>
2026-09-20 02:28:32 -0400

    Time: 4 ms (47.75%), Space: 21.4 MB (13.05%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+
+        preMap = {i:[] for i in range(numCourses)}
+
+        for pre, crs in prerequisites:
+            preMap[pre].append(crs)
+        
+        visit = set()
+        cycle = set()
+        output = []
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            
+            if crs in visit:
+                return True
+            
+            cycle.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visit.add(crs)
+            cycle.remove(crs)
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+
+        return output
+        
